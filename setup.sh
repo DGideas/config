030c80bbe3c8b4a488b8984b2766cb3bc05c523d
王万霖 <dgideas@outlook.com>
2017-01-29 02:11:39 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+apt-get update
+apt-get dist-upgrade -y
+apt-get autoremove --purge -y
+apt-get install gimp inkscape cowsay sl -y
+apt-get install php apache2 libapache2-mod-php7.0 -y
+apt-get install docker.io -y
+apt-get install python-opencv libcv2.4 libcv-dev libcvaux2.4 libcvaux-dev
+apt-get install flashplugin-installer -y
