ac1311cc4f14694a956e4d8ba1d04008897da4d3
王万霖 <dgideas@outlook.com>
2019-01-22 23:24:20 +0800

    Create rabbitmq.sh

---
+++ rabbitmq.sh
@@ -0,0 +1,4 @@
+# https://stackoverflow.com/questions/23669780/rabbitmq-3-3-1-can-not-login-with-guest-guest
+rabbitmqctl add_user test test
+rabbitmqctl set_user_tags test administrator
+rabbitmqctl set_permissions -p / test ".*" ".*" ".*"
