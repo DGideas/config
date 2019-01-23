069b2e260289ce31711be1e77816ea67b7cb002e
王万霖 <dgideas@outlook.com>
2019-01-23 10:58:56 +0800

    Update rabbitmq.sh

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,4 +1,5 @@
 # https://stackoverflow.com/questions/23669780/rabbitmq-3-3-1-can-not-login-with-guest-guest
+rabbitmq-plugins enable rabbitmq_management
 rabbitmqctl add_user test test
 rabbitmqctl set_user_tags test administrator
 rabbitmqctl set_permissions -p / test ".*" ".*" ".*"
