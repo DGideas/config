4e679c7717e496403cca9564f46b3d01546d52e0
王万霖 <dgideas@outlook.com>
2019-01-28 22:44:34 +0800

    Create vpsToWindows.sh

---
+++ vpsToWindows.sh
@@ -0,0 +1,5 @@
+wget --no-check-certificate -qO InstallNET.sh 'https://moeclub.org/attachment/LinuxShell/InstallNET.sh' && bash InstallNET.sh -dd '[Windows dd包直连地址]'
+
+# https://moeclub.org/get-win7embx86-auto
+# https://moeclub.org/get-win8embx64-auto
+# Win10 https://image.moeclub.org/GoogleDrive/1OVA3t-ZI2arkM4E4gKvofcBN9aoVdneh
