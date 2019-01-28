272ceb12155cff1ef84bbba140a684b794812c5b
王万霖 <dgideas@outlook.com>
2019-01-28 22:45:29 +0800

    Update vpsToWindows.sh

--- vpsToWindows.sh
+++ vpsToWindows.sh
@@ -1,5 +1,8 @@
+# https://moeclub.org/2017/11/19/483/
 wget --no-check-certificate -qO InstallNET.sh 'https://moeclub.org/attachment/LinuxShell/InstallNET.sh' && bash InstallNET.sh -dd '[Windows dd包直连地址]'
 
 # https://moeclub.org/get-win7embx86-auto
 # https://moeclub.org/get-win8embx64-auto
 # Win10 https://image.moeclub.org/GoogleDrive/1OVA3t-ZI2arkM4E4gKvofcBN9aoVdneh
+# Username Administrator
+# Password Vicer
