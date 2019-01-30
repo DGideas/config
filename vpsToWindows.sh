205a15e6846e6526c09547550dffe2d1e7fac5df
王万霖 <dgideas@outlook.com>
2019-01-30 14:45:10 +0800

    Update vpsToWindows.sh

--- vpsToWindows.sh
+++ vpsToWindows.sh
@@ -1,6 +1,6 @@
 # https://moeclub.org/2017/11/19/483/
 wget --no-check-certificate -qO InstallNET.sh 'https://moeclub.org/attachment/LinuxShell/InstallNET.sh' \
-  && bash InstallNET.sh -dd 'https://image.moeclub.org/GoogleDrive/1OVA3t-ZI2arkM4E4gKvofcBN9aoVdneh'
+  && bash InstallNET.sh -dd 'https://moeclub.org/get-win7embx86-auto'
 
 # https://moeclub.org/get-win7embx86-auto
 # https://moeclub.org/get-win8embx64-auto
