1569b3a5f0bd373f56313adea5ca788996bb917a
王万霖 <dgideas@outlook.com>
2019-02-19 16:00:15 +0800

    Update one_key_bbr.sh

--- one_key_bbr.sh
+++ one_key_bbr.sh
@@ -1,4 +1,4 @@
-# 
+# wget -qO- https://raw.githubusercontent.com/DGideas/config/master/one_key_bbr.sh | bash
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 sysctl -p
