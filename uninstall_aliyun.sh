f9695ca11086e614ea4e64e6a5613b7c1c52a1c0
王万霖 <dgideas@outlook.com>
2019-04-02 11:04:36 +0800

    Update uninstall_aliyun.sh

--- uninstall_aliyun.sh
+++ uninstall_aliyun.sh
@@ -6,17 +6,9 @@ wget -qO- http://update.aegis.aliyun.com/download/quartz_uninstall.sh | bash
 pkill aliyun-service
 rm -fr /etc/init.d/agentwatch /usr/sbin/aliyun-service
 rm -rf /usr/local/aegis*
-iptables -I INPUT -s 140.205.201.0/28 -j DROP
-iptables -I INPUT -s 140.205.201.16/29 -j DROP
-iptables -I INPUT -s 140.205.201.32/28 -j DROP
-iptables -I INPUT -s 140.205.225.192/29 -j DROP
-iptables -I INPUT -s 140.205.225.200/30 -j DROP
-iptables -I INPUT -s 140.205.225.184/29 -j DROP
-iptables -I INPUT -s 140.205.225.183/32 -j DROP
-iptables -I INPUT -s 140.205.225.206/32 -j DROP
-iptables -I INPUT -s 140.205.225.205/32 -j DROP
-iptables -I INPUT -s 140.205.225.195/32 -j DROP
-iptables -I INPUT -s 140.205.225.204/32 -j DROP
 rm -rf /usr/local/aegis
 rm /usr/sbin/aliyun-service
 rm /lib/systemd/system/aliyun.service
+rm -rf /usr/local/cloudmonitor
+deluser admin --remove-all-files -q
+rm -rf /usr/lib/accountsservice
