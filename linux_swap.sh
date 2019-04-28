e1edef71180037acbd2a6d7aa93b2e237ab846bf
王万霖 <dgideas@outlook.com>
2019-04-28 23:20:32 +0800

    Update linux_swap.sh

--- linux_swap.sh
+++ linux_swap.sh
@@ -1,9 +1,10 @@
-# Create Swapfile on Linux System
-# Memory(RAM) is so expensive...
+# Create Swapfile on Linux System(2G Default)
+# wget -qO- https://raw.githubusercontent.com/DGideas/config/master/linux_swap.sh | bash
 cd /mnt
-# Create 16GiB of swap space
-dd if=/dev/zero of=swapfile bs=1M count=16384
+# Create 2GiB of swap space
+dd if=/dev/zero of=swapfile bs=1M count=2048
 mkswap swapfile
+chmod 0600 /mnt/swapfile
 swapon swapfile
 # When you need use swapfile on every startup, write the line below into /etc/fstab (noinclude slash#)
-# /mnt/swapfile none swap defaults 0 0
+echo '/mnt/swapfile none swap defaults 0 0' >> /etc/fstab
