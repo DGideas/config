41652529ed0b500a9d748477fded62c2a997bafb
王万霖 <dgideas@outlook.com>
2020-01-15 16:25:03 +0800

    Create linux_swap_tiny.sh

---
+++ linux_swap_tiny.sh
@@ -0,0 +1,10 @@
+# Create Swapfile on Linux System (2G)
+# wget -qO- https://raw.githubusercontent.com/DGideas/config/master/linux_swap_tiny.sh | bash
+cd /mnt
+# Create 2GiB of swap space
+dd if=/dev/zero of=swapfile bs=1M count=2048
+mkswap swapfile
+chmod 0600 /mnt/swapfile
+swapon swapfile
+# When you need use swapfile on every startup, write the line below into /etc/fstab (noinclude slash#)
+echo '/mnt/swapfile none swap defaults 0 0' >> /etc/fstab
