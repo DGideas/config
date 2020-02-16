a0276398965f7ce6cbc3289467aae8f69d8a69f7
王万霖 <dgideas@outlook.com>
2020-02-16 15:58:11 +0800

    Update install_manim.sh

--- install_manim.sh
+++ install_manim.sh
@@ -3,7 +3,11 @@
 # Need root privilege
 apt-get update
 apt-get dist-upgrade -y
-apt-get install libcairo2-dev ffmpeg sox texlive-latex-extra -y
+# apt-get install libcairo2-dev ffmpeg sox texlive-latex-extra -y
+apt-get install libcairo2-dev -y
+pip3 install latex
+pip3 install sox
+pip3 install ffmpeg
 pip3 install manimlib
 apt-get autoremove --purge -y
 apt-get clean
