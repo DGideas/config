785d5d9d512860f5c4c1b6b4c3714c7d5070a31e
王万霖 <dgideas@outlook.com>
2020-03-24 10:36:56 +0800

    Update gnome-workspace-optimize.sh

--- gnome-workspace-optimize.sh
+++ gnome-workspace-optimize.sh
@@ -4,3 +4,5 @@
 # gsettings set org.gnome.shell.window-switcher current-workspace-only true
 gsettings set org.gnome.shell.app-switcher current-workspace-only true
 gsettings set org.gnome.shell.extensions.dash-to-dock isolate-workspaces true
+
+gsettings set org.gnome.nautilus.desktop trash-icon-visible false
