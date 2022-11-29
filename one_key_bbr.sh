dc2dd682e68b3533f8233e16d3eb13ed89540f0a
Wanlin Wang <dgideas@outlook.com>
2022-11-29 11:49:57 +0800

    add tcp_notsent_lowat config
    According https://blog.cloudflare.com/http-2-prioritization-with-nginx/ to configure `tcp_notsent_lowat` settings

--- one_key_bbr.sh
+++ one_key_bbr.sh
@@ -1,5 +1,6 @@
 # wget -qO- https://raw.githubusercontent.com/DGideas/config/master/one_key_bbr.sh | bash
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_notsent_lowat=16384" >> /etc/sysctl.conf
 sysctl -p
 lsmod | grep bbr
